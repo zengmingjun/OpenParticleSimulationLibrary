71a34f76b539c22bc7b9d2cb830a655a26d81d1c
zengmingjun <132594355+zengmingjun@users.noreply.github.com>
2024-01-19 02:18:50 +0800

    Add files via upload

---
+++ include/comp.h
@@ -0,0 +1,140 @@
+#include"def.h"
+#include<iostream>
+#include<vector>
+#include<memory>
+
+using namespace std;
+
+namespace sp {
+
+#ifdef SPACE_3D
+
+	class LONGDOU
+	{
+	public:
+		long double Arr[3] = { NULL };
+	};
+
+	class DOU
+	{
+	public:
+		double Arr[3] = { NULL };
+	};
+
+	class UNSI
+	{
+	public:
+		unsigned Arr[3] = {NULL};
+	};
+	class PartIndex
+	{
+	public:
+		unsigned PartCor[3] = {NULL};
+		unsigned Index = 0;
+	};
+
+#endif
+
+#ifndef SPACE_3D
+
+	class LONGDOU
+	{
+	public:
+		long double Arr[2] = { NULL };
+	};
+
+	class DOU
+	{
+	public:
+		double Arr[2] = { NULL };
+	};
+
+	class UNSI
+	{
+	public:
+		unsigned Arr[2] = { NULL };
+	};
+
+	class PartIndex
+	{
+	public:
+		unsigned PartCor[2] = { NULL };
+		unsigned Index = 0;
+		
+	};
+#endif
+
+	class Point
+	{
+	public:
+#ifndef HVAE_NO_NUMBERING
+		long long Numbering;
+#endif // !HVAE_NO_NUMBERING
+		PartIndex partindex;
+		unsigned PartGrpIndex;
+		LONGDOU Coor;
+		DOU F;
+		DOU V;
+		Point();
+		~Point();
+	};
+
+	class Part
+	{
+	public:
+		vector<Point*>pPotGrp;
+		Part();
+		~Part();
+	};
+
+	typedef void(*FUPFUC)(Point& selfFData, Point* GestFData,double DisPowTwo);
+	typedef void(*COORUPFUC)(Point& self);
+	typedef void(*SETCOORFUC)(Point& self);
+
+	class SilY
+	{
+	public:
+		unsigned NumZ;
+
+	};
+
+	class SilX
+	{
+	public:
+		unsigned NumY;
+		SilY* GrpY;
+		SilX();
+		~SilX();
+	};
+
+	class Sil
+	{
+	public:
+		unsigned NumX;
+		SilX* GrpX;
+		Sil();
+		~Sil();
+	};
+
+	
+
+	/*extern unsigned Dimension;
+	extern vector<unsigned> PartNum;
+	extern vector<long long> Vensize;
+	extern vector<unsigned>PartIndexUint;
+	extern unique_ptr<Part[]> pPart;*/
+	extern vector<Point>PotGrp;
+	void CreateSpace(vector<unsigned> partCount,vector<long long> VenSize, unsigned pointvision);
+	void AddPointbat(long long PointCount, long long BaseNumbering);
+	void setCoor(SETCOORFUC fuc);
+	void AddInPart();
+	void setFUpFuc(FUPFUC fuc);
+	void setCoorUpFuc(COORUPFUC fuc);
+	void SpaceDataUpUesPotGrp();
+	void SpaceDataUpUesPartGrp();
+
+	void IndexParse(PartIndex& partcoor, LONGDOU coor);
+	double ThrDistancePow(LONGDOU hcoor, LONGDOU gcoor);
+	double Distance(double xed, double x);
+	void DestroyedSpace();
+}
---
+++ include/def.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define BasePartGrp 10
+#define MAX_GRPCOUNT 100
+
+#define dt 0.1
+#define Kmf 0.1
+
+//#define SPACE_3D
+//#define HVAE_NO_NUMBERING
+//#define ERORR_OVERRANGE
